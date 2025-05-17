122277c7afb4b46cafdbd2ebcc3650f24110a366
Stefanie Wedel <wedel.stefanie@protonmail.com>
2025-05-17 19:53:05 +0200

    Add app.py to root and renamed app_pages/ folder

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+intro_page = st.Page("app_pages/page_1_intro.py", title="Introduction to ReadingQuest")
+reading_bingo = st.Page("app_pages/page_2_reading_bingo.py", title="Reading Bingo")
+challenge_matching = st.Page("app_pages/page_3_challenge_matching.py", title="Challenge Matching")
+
+pg = st.navigation([intro_page, reading_bingo, challenge_matching])
+st.set_page_config(page_title="ReadingQuest")
+pg.run()
