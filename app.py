d69eca2a8d7aa3acc12fe64e5809cde500457066
Stefanie Wedel <wedel.stefanie@protonmail.com>
2025-07-09 15:37:28 +0200

    Update Name to NaraLune in Page Title

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ reading_bingo = st.Page("app_pages/page_2_reading_bingo.py", title="Reading Bing
 challenge_matching = st.Page("app_pages/page_3_challenge_matching.py", title="Challenge Matching")
 
 pg = st.navigation([intro_page, reading_bingo, challenge_matching])
-st.set_page_config(page_title="ReadingQuest")
+st.set_page_config(page_title="NarraLune")
 pg.run()
